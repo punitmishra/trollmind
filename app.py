d1880d5fd4e1701baa714df27b3246fd6a98b3eb
Punit Mishra <punit.mishra09@gmail.com>
2020-12-28 16:34:02 -0800

    Static Files Test

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from typing import Optional
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root():
@@ -16,5 +21,10 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 
 @app.get("/troll")
-def troll_miind():
+def troll_mind():
     return {"troll": "Great! Aliens have taken over :troll"}
+
+
+@app.get("/rs", response_class=HTMLResponse)
+async def read_static(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
