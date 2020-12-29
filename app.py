0eadefc8f22da2a54f61f973e092a62abee81627
Punit Mishra <punit.mishra09@gmail.com>
2020-12-28 16:49:05 -0800

    Fixing the test static param

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -25,6 +26,6 @@ def troll_mind():
     return {"troll": "Great! Aliens have taken over :troll"}
 
 
-@app.get("/rs", response_class=HTMLResponse)
+@app.get("/rs/{id}", response_class=HTMLResponse)
 async def read_static(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
